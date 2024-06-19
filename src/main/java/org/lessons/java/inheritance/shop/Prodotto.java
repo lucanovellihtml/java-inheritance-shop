97e6e42cdeaf1090a4274cf4e4af6cf26014eaae
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-06-19 11:59:57 +0200

    add: added superclass "Prodotto.java"

---
+++ src/main/java/org/lessons/java/inheritance/shop/Prodotto.java
@@ -0,0 +1,66 @@
+package org.lessons.java.inheritance.shop;
+
+
+//SUPERCLASSE
+public class Prodotto {
+
+	//VARIABILI
+	protected int code;
+	protected String name, type;
+	protected double price, iva;
+	
+	
+	//COSTRUTTORE
+	public Prodotto(String name, String type, double price, double iva) {
+		this.code = (int) (Math.random() * 1000);
+		this.name = name;
+		this.type = type;
+		this.price = price;
+		this.iva = iva;
+	}
+
+	
+	//METODI GETTER/SETTER
+	public int getCode() {
+		return code;
+	}
+
+	//NON ACCESSIBILE
+	private void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String gType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	//METODO PER OTTENERE PREZZO BASE
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public double getIva() {
+		return iva;
+	}
+
+	public void setIva(double iva) {
+		this.iva = iva;
+	}
+		
+}
